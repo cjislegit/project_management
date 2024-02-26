7c5d32a4e01bf21a16b280092f65af0b4743fa21
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-02-26 15:34:32 -0800

    Started on sidebard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Sidebar from './components/Sidebard';
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <h1 className='my-8 text-center text-5xl font-bold'>Hello World</h1>
+      <Sidebar />
     </>
   );
 }
---
+++ src/components/Sidebard.jsx
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+  return (
+    <aside className='bg-stone-950 text-stone-100'>
+      <div>You Projects</div>
+      <button className='bg-stone-700 text-stone-300'>+ Add Project</button>
+    </aside>
+  );
+};
+
+export default Sidebar;
