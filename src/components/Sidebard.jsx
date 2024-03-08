d8dada1a784ef2a50dc5be9ec87c6294d70659b3
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-03-07 21:13:13 -0800

    Created component to show when no project has been selected and created a button component

--- src/components/Sidebard.jsx
+++ src/components/Sidebard.jsx
@@ -1,3 +1,5 @@
+import Button from './Button';
+
 const Sidebar = () => {
   return (
     <aside className='bg-stone-900 text-stone-50 w-1/3 px-8 py-16 md:w-72 rounded-r-xl'>
@@ -5,9 +7,7 @@ const Sidebar = () => {
         You Projects
       </h2>
       <div>
-        <button className='px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100'>
-          + Add Project
-        </button>
+        <Button>+ Add Project</Button>
       </div>
       <ul></ul>
     </aside>
