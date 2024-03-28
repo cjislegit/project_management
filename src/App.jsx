94430f65712e9520656aef2b89d394c786c35341
Carlos Ramirez <carlosjramirez11@gmail.com>
2024-03-28 14:44:09 -0700

    Built modal

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,41 @@ function App() {
     });
   };
 
+  const handleCancel = () => {
+    setProjectsState((prevState) => {
+      return { ...prevState, selectedProjectId: undefined };
+    });
+  };
+
+  const handleAddProject = (projectData) => {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  };
+
   let content;
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancel} />;
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <Sidebar onStartAddProject={handleStartAddProject} />
+      <Sidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
